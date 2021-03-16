b4273ada820eed807c2454575605a3e585724f6d
alberto garcia elias <agarcweb@gmail.com>
2021-03-16 09:48:56 +0100

    installation and test of SQLite native

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import 'react-native-gesture-handler';
 import {StatusBar} from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from "./Routes";
 import SplashScreen from "./screens/SplashScreen";
+import * as SQLite from 'expo-sqlite';
 
-
+const db = SQLite.openDatabase("appbobbins.db");
 
 function App() {
 
@@ -24,7 +25,7 @@ function App() {
                 loadScreen ?
                     <SplashScreen/>
                     :
-                        <Routes/>
+                    <Routes/>
             }
         </NavigationContainer>
     );
