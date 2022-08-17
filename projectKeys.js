1714d106068ce1d70785a6be319fcaeaaebcd4ad
albertogarel <agarcweb@gmail.com>
2022-08-18 00:36:07 +0200

    delete files

--- projectKeys.js
+++
@@ -1,6 +0,0 @@
-//SENTRTY DSN
-export const SENTRY_DSN = "https://ac08099c74aa429aa10998fe25aa3b1e@o1329653.ingest.sentry.io/6591893";
-export const ADMOB_KEYS = {
-    "android_app_id": "ca-app-pub-5632250524548972~7879947023",
-    "admod_SDK": 'ca-app-pub-5632250524548972/2521192025'
-};
